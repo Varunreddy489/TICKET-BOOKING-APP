5e03c621a2b088c4810c37265a1a9b8048db36b2
Varunreddy <varunsannapureddy@gmail.com>
2025-03-11 11:22:46 +0530

    Add type definitions for genres, user, movie, ticket, and review

---
+++ client/src/types/types.d.ts
@@ -0,0 +1,63 @@
+export const Genres = {
+  Action: "Action",
+  Adventure: "Adventure",
+  Comedy: "Comedy",
+  Drama: "Drama",
+  Fantasy: "Fantasy",
+  Horror: "Horror",
+  Mystery: "Mystery",
+  Romance: "Romance",
+  Thriller: "Thriller",
+  Western: "Western",
+  SciFi: "Sci-Fi",
+  Animation: "Animation",
+};
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  clerkId: string;
+  role: string;
+  genres: Genres[];
+};
+
+export type Movie = {
+  id: number;
+  title: string;
+  description: string;
+  rating: number;
+  images: string[];
+  ticketCost: number;
+  timings: string[];
+  languages: string[];
+  genres: Genres[];
+  isMovieAvailable: boolean;
+  movieSeatCapacity: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  reviews?: Review[];
+};
+
+export type Ticket = {
+  id?: number;
+  count: number;
+  cost: number;
+  totalCost?: number;
+  userId?: string | null;
+  timing: string;
+  seatNumber: string[];
+  movieId: number;
+  isExpired?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type Review = {
+  id?: number;
+  content: string;
+  rating: number;
+  movieId: number;
+  userId: number;
+  createdAt?: Date;
+};
