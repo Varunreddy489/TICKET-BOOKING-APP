95d5efdeece3cf6000fd18d64b4a7a9c4162b09a
Varunreddy <varunsannapureddy@gmail.com>
2025-03-16 16:14:03 +0530

    Add ThemeProvider to main component and update .gitignore for imdbApi.js

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import AuthProvider from "./providers/AuthProvider.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -16,7 +17,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <AuthProvider>
       <BrowserRouter>
-        <App />
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
   </ClerkProvider>
