ef8f8bd05a77578f97275e0d93ea78f5f42df5c2
Varunreddy <varunsannapureddy@gmail.com>
2025-03-18 10:50:36 +0530

    Add wallet and wallet transaction models with migrations and update seed script

---
+++ server/prisma/migrations/20250318044557_add_wallet/migration.sql
@@ -0,0 +1,28 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('ADD', 'DEDUCT', 'REFUND');
+
+-- CreateTable
+CREATE TABLE "Wallet" (
+    "id" TEXT NOT NULL,
+    "balance" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Wallet_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WalletTransaction" (
+    "id" TEXT NOT NULL,
+    "walletId" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WalletTransaction_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Wallet" ADD CONSTRAINT "Wallet_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WalletTransaction" ADD CONSTRAINT "WalletTransaction_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallet"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
