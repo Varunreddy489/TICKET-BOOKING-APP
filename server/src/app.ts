13723ff25aca406d0905c01dc738fe50401093bb
Varunreddy <varunsannapureddy@gmail.com>
2025-03-20 14:29:46 +0530

    Add express-rate-limit middleware and configure rate limiting

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 import cors from "cors";
 import express from "express";
-
-import { apiRoutes } from "./routes";
 import { clerkMiddleware } from "@clerk/express";
+
+import { limiter } from "./config";
+import { apiRoutes } from "./routes";
 
 const app = express();
 
 app.use(cors());
+app.use(limiter);
 app.use(express.json());
 app.use(clerkMiddleware());
 app.use(express.static("public"));
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,3 +1,5 @@
 export * from "./server-config";
 export * from "./logger.config";
-export { default as prisma } from "./prisma.config";
+export * from "./prisma.config";
+export * from "./rateLimiter.config";
+// export { default as prisma } from "./prisma.config";
